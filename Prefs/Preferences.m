b6aabf5de5a05a4fba3caf225a4417841ca767aa
Nepeta <ominousness@protonmail.com>
2019-04-13 19:03:22 +0200

    return nil on invalid base64

--- Prefs/Preferences.m
+++ Prefs/Preferences.m
@@ -123,6 +123,8 @@
 
 -(NSDictionary*)deserializeDictionary:(NSString *)string {
     NSData *plist = [[NSData alloc] initWithBase64EncodedString:string options:kNilOptions];
+    if !(plist) return nil;
+    
     return [NSPropertyListSerialization propertyListWithData:plist
                  options:kNilOptions
                   format:NULL
