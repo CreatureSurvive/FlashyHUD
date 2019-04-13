b1d6572600d493b7675b7d91500db2f23cfa7201
Nepeta <ominousness@protonmail.com>
2019-04-13 16:45:36 +0200

    presets

--- Prefs/Preferences.h
+++ Prefs/Preferences.h
@@ -7,8 +7,19 @@
 
 #define COLORS_PATH @"/var/mobile/Library/Preferences/me.nepeta.flashyhud-colors.plist"
 #define BUNDLE_ID @"me.nepeta.flashyhud"
+#define TWEAK_NAME @"FlashyHUD"
 
 @interface FLHPrefsListController : HBRootListController
     - (void)resetPrefs:(id)sender;
     - (void)respring:(id)sender;
+    - (void)saveSettings:(id)sender;
+    - (void)shareSettings:(id)sender;
+    - (void)importSettings:(id)sender;
+    - (void)addDictionaryToSavedSettings:(NSDictionary *)dictionary;
+    - (void)restoreSettingsFromDictionary:(NSDictionary*)settings;
+    - (void)removeSavedSettingsAtIndex:(int)i;
+    - (void)renameSavedSettingsAtIndex:(int)i name:(NSString*)name;
+    - (NSDictionary*)dictionaryWithCurrentSettingsAndName:(NSString*)name;
+    - (NSString*)serializeDictionary:(NSDictionary *)dictionary;
+    - (NSDictionary*)deserializeDictionary:(NSString *)string;
 @end
